a78525316131a66572bbbed288fb250322046680
Jay-pixe <75147846+Jay-pixe@users.noreply.github.com>
2024-09-25 15:39:43 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// Add any interactivity here (optional)
+// Example: Smooth Scroll for navigation
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
